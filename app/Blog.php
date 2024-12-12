fbda8334fbb4124ef947df4719f7778b0bb7e1fe
Suraj Bhandari <surajbhandari5110@gmail.com>
2024-12-12 18:02:15 +0530

    done blogs

---
+++ app/Blog.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Blog extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'blogs';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'by_user',
+        'comment',
+        'primary_image',
+        'secondary_image',
+        'moto1',
+        'para1',
+        'moto2',
+    ];
+
+    
+}
