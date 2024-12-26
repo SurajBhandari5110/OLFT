4074e30bd5cd5d7fcedd66d75ad972bac75701c5
Suraj Bhandari <surajbhandari5110@gmail.com>
2024-12-26 17:35:03 +0530

    added apis

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,16 @@ use App\Http\Controllers\ItinerariesController;
 use App\Http\Controllers\GalleryController;
 use App\Http\Controllers\ClientQueryController;
 
+
+use App\Http\Controllers\PackageStayController;
+
+use App\Http\Controllers\StaysController;
+use App\Http\Controllers\DestinationController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\TagController;
+use App\Http\Controllers\BlogController;
+
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -37,3 +47,19 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('client_queries/store', [ClientQueryController::class, 'store'])->name('client_queries.store');
+//Public Routes
+Route::get('/', [TourController::class, 'index']);
+Route::get('footer', [ContactController::class, 'showFooter'])->name('footer.show');
+Route::get('package/{id}', [PackageController::class, 'show'])->name('package.show');
+Route::get('tourguide_data', [TourGuideController::class, 'show'])->name('tourguides.show');
+Route::get('galleries/package/{package_id}', [GalleryController::class, 'fetchByPackageId']);
+Route::get('/destinations/country/{country}', [DestinationController::class, 'getDestinationsByCountry'])->name('destinations.byCountry');
+Route::get('/packages/country/{country}', [PackageController::class, 'getPackagesByCountry']);
+
+//fetching by categry and tags
+Route::get('/category/{name}/', [CategoryController::class, 'fetchPackagesByCategory'])->name('categories.fetchBySlug');
+Route::get('/tag/{tag}/', [TagController::class, 'fetchPackagesByTag'])->name('tags.fetchBySlug');
+
+//fetching blogs
+Route::get('/blogs_details', [BlogController::class, 'fetchBlog']); // Display all blogs
+Route::get('/blogs_details/{id}', [BlogController::class, 'show'])->name('blogs.show');
