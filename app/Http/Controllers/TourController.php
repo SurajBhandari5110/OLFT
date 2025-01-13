cf404b1a1ffd5e3742d0b1e00b9575417c605fe0
Suraj Bhandari <surajbhandari5110@gmail.com>
2025-01-13 15:53:54 +0530

    Updated API for search fields in frontend

--- app/Http/Controllers/TourController.php
+++ app/Http/Controllers/TourController.php
@@ -8,7 +8,7 @@ class TourController extends Controller
 {
     public function index()
     {
-    $packages = Package::select('pk_Package_id','title', 'duration', 'image')->get();
+    $packages = Package::select('pk_Package_id','title', 'duration', 'image','country','tour_type')->get();
     return response()->json($packages);
     // return view('index', compact('packages'));
     }
